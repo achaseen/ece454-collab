04f33b9a7d025780fcf1d9aae2f52c143ce73812
danmasse <danmasse@gmail.com>
2015-07-01 03:31:37 -0400

    Got rid of unneeded edgeList and lceaned up a bit.

--- runall.sh
+++ runall.sh
@@ -34,17 +34,17 @@ perl sorted/sort.pl output.txt > new_list
 echo ------
 diff new_list sorted/graph1K_B
 echo ------
-java -Xmx4096m -cp a2.jar ece454750s15a2.TriangleCount -ncores 2 -if inputs/graph1M_B.txt -of output.txt
+java -cp a2.jar ece454750s15a2.TriangleCount -ncores 2 -if inputs/graph1M_B.txt -of output.txt
 perl sorted/sort.pl output.txt > new_list
 echo ------
 diff new_list sorted/graph1M_B
 echo ------
-java -Xmx4096m -cp a2.jar ece454750s15a2.TriangleCount -ncores 2 -if inputs/graph100K_C.txt -of output.txt
+java -cp a2.jar ece454750s15a2.TriangleCount -ncores 2 -if inputs/graph100K_C.txt -of output.txt
 perl sorted/sort.pl output.txt > new_list
 echo ------
 diff new_list sorted/graph100K_C
 echo ------
-java -Xmx4096m -cp a2.jar ece454750s15a2.TriangleCount -ncores 2 -if inputs/graph10K_C.txt -of output.txt
+java -cp a2.jar ece454750s15a2.TriangleCount -ncores 2 -if inputs/graph10K_C.txt -of output.txt
 perl sorted/sort.pl output.txt > new_list
 echo ------
 diff new_list sorted/graph10K_C
--- src/ece454750s15a2/TriangleCountImpl.java
+++ src/ece454750s15a2/TriangleCountImpl.java
@@ -30,95 +30,47 @@ public class TriangleCountImpl {
 	    // this code is single-threaded and ignores numCores
     
         long startTime = System.currentTimeMillis();
-        ArrayList<ArrayList<Integer>> edgeList = getEdgeList(input);
         ArrayList<ArrayList<Integer>> adjacencyList = getAdjacencyList(input);
         long endTime = System.currentTimeMillis();
         long diffTime = endTime - startTime;
-	    System.out.println("Lists obtained in " + diffTime/1000 + "s");
+	    System.out.println("Lists obtained in " + diffTime);
 
 	    ArrayList<Triangle> ret = new ArrayList<Triangle>();
 
-	    long loops1 = 0, loops2 = 0, loops3 = 0;
         startTime = System.currentTimeMillis();
-        int numEdges = edgeList.size();
-        for (int i = 0; i < numEdges-1; i++) {
-        	loops1++;
-        	boolean vertex1_match = true;
-        	ArrayList<Integer> edge1 = edgeList.get(i);
-        	int vertex1 = edge1.get(0);
-        	int vertex2 = edge1.get(1);
+        
+        for (int i=0; i < adjacencyList.size()-1; i++) {
         	
-        	int count = i+1;
-        	while(vertex1_match){
-        		loops2++;
-        		ArrayList<Integer> edge2 = edgeList.get(count);
-        		if( edge2.get(0) == vertex1 ) {
-        			int vertex3 = edge2.get(1);
-        			// Two edges share a common vertex
-        			// Find matching edge
-        			if( adjacencyList.get(vertex2).contains(vertex3) ) {
-        				if(vertex2<vertex3) {
-        					ret.add( new Triangle(vertex1, vertex2, vertex3) );
-        				}else{
-        					ret.add( new Triangle(vertex1, vertex3, vertex2) );
-        				}
-        			}
-        		}else{
-        			vertex1_match = false;
-        			break;
-        		}
-        		count++;
-        		if(count+1 >= numEdges ){
-        			vertex1_match = false;
+        	int vertex1 = i;
+        	
+        	for(int j=0; j < adjacencyList.get(i).size()-1; j++) {
+        		int vertex2 = adjacencyList.get(i).get(j);
+        		if (vertex1 < vertex2) {
+        			int count = j+1;
+        		
+        			boolean same_vertex = true;
+        			while(same_vertex) {
+	        			int vertex3 = adjacencyList.get(i).get(count);
+	        			if( adjacencyList.get(vertex2).contains(vertex3) ) {
+	        				ret.add( new Triangle(vertex1, vertex2, vertex3) );
+	        			}
+	
+	        			count++;
+	        			if(count >= adjacencyList.get(i).size() ){
+	        				same_vertex = false;
+	        			}
+	        		}
         		}
         	}
         }
         
         endTime = System.currentTimeMillis();
         diffTime = endTime - startTime;
-	    System.out.println("Count obtained in " + diffTime/1000 + "s");
-	    System.out.println("loop1: " + loops1 + " loops2: " + loops2 + " loops3: " +loops3);
+	    System.out.println("Count obtained in " + diffTime);
 	    
         return ret;
     }
 
-    public ArrayList<ArrayList<Integer>> getEdgeList(byte[] data) throws IOException {
-	    InputStream istream = new ByteArrayInputStream(data);
-	    BufferedReader br = new BufferedReader(new InputStreamReader(istream));
-
-	    String strLine = br.readLine();
-	    if (!strLine.contains("vertices") || !strLine.contains("edges")) {
-	        System.err.println("Invalid graph file format. Offending line: " + strLine);
-	        System.exit(-1);	    
-	    }
-	    String parts[] = strLine.split(", ");
-	    int numVertices = Integer.parseInt(parts[0].split(" ")[0]);
-	    int numEdges = Integer.parseInt(parts[1].split(" ")[0]);
-	    System.out.println("Found graph with " + numVertices + " vertices and " + numEdges + " edges");
-
-	    ArrayList<ArrayList<Integer>> edgeList = new ArrayList<ArrayList<Integer>>(numEdges);
-	    
-        while ((strLine = br.readLine()) != null && !strLine.equals(""))   {
-	        parts = strLine.split(": ");
-	        int vertex1 = Integer.parseInt(parts[0]);
-	        if (parts.length > 1) {
-		        parts = parts[1].split(" ");
-		        if ( parts.length > 1 ) {
-		        	for (String part: parts) {
-		        		if( vertex1 < Integer.parseInt(part) ) {
-		        			ArrayList<Integer> myList = new ArrayList<Integer>();
-		        			myList.add(vertex1);
-		        			myList.add(Integer.parseInt(part));
-		        			edgeList.add(myList);
-		        		}
-		        	}
-		        }
-	        }
-        }
-	    br.close();
-	    return edgeList;
-    }
-    
     public ArrayList<ArrayList<Integer>> getAdjacencyList(byte[] data) throws IOException {
 	    InputStream istream = new ByteArrayInputStream(data);
 	    BufferedReader br = new BufferedReader(new InputStreamReader(istream));
