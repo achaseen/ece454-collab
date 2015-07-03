0fac0123624e1d40cf9ed66cdf3e15211c2b861e
danmasse <danmasse@gmail.com>
2015-07-03 19:40:39 -0400

    Update to add hashset instead of arraylist

--- src/ece454750s15a2/TriangleCountImpl.java
+++ src/ece454750s15a2/TriangleCountImpl.java
@@ -11,6 +11,11 @@ package ece454750s15a2;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 
 public class TriangleCountImpl {
     private byte[] input;
@@ -24,54 +29,141 @@ public class TriangleCountImpl {
     public List<String> getGroupMembers() {
 	    ArrayList<String> ret = new ArrayList<String>();
 	    ret.add("dmjmasse");
+	    ret.add("m5ji");
+	    ret.add("achaseen");
 	    return ret;
     }
     public List<Triangle> enumerateTriangles() throws IOException {
 	    // this code is single-threaded and ignores numCores
-    
-        long startTime = System.currentTimeMillis();
-        ArrayList<ArrayList<Integer>> adjacencyList = getAdjacencyList(input);
-        long endTime = System.currentTimeMillis();
-        long diffTime = endTime - startTime;
-	    System.out.println("Lists obtained in " + diffTime);
+    	
+    	ArrayList<Triangle> ret = new ArrayList<Triangle>();
+    	
+        if(numCores < 2) {
+        	long startTime = System.currentTimeMillis();
+            ArrayList<HashSet<Integer>> adjacencyList = getSingleAdjacencyList(input);
+            long endTime = System.currentTimeMillis();
+            long diffTime = endTime - startTime;
+    	    System.out.println("Lists obtained in " + diffTime);    	    
 
-	    ArrayList<Triangle> ret = new ArrayList<Triangle>();
+            startTime = System.currentTimeMillis();
+	        for (int i=0; i < adjacencyList.size()-1; i++) {
+	        	       	
+	            int vertex1 = i;
+	            if(adjacencyList.get(vertex1).size() > 1){
+	            	Object[] vertex1Array = adjacencyList.get(i).toArray();
+	            	for(int j=0; j < vertex1Array.length; j++) {
+	        		
+	            		int vertex2 = (int)vertex1Array[j];
+	            		if(adjacencyList.get(vertex2).size() > 1){
+	            			if (vertex1 < vertex2) {
+	        			
+	            				for(int count=j+1; count<vertex1Array.length; count++) {        			
+	            					int vertex3 = (int)vertex1Array[count];
+	            					if(vertex1 < vertex3){
+	            						if( adjacencyList.get(vertex2).contains(vertex3) ) {
+	            							if( vertex2 < vertex3 ) {
+	            								ret.add( new Triangle(vertex1, vertex2, vertex3) );
+	            							}else{
+	            								ret.add( new Triangle(vertex1, vertex3, vertex2) );
+	            							}
+	            						}
+	            					}        			
+	            				}
+	            			}
+	            		}
+	            	}
+	            }
+	        }
+	        endTime = System.currentTimeMillis();
+	        diffTime = endTime - startTime;
+		    System.out.println("Count obtained in " + diffTime);
+	    
+    	}else{
+    		// parallelized version
+    		ExecutorService pool = Executors.newFixedThreadPool(numCores);
+            long startTime = System.currentTimeMillis();
+            ArrayList<Set<Integer>> adjacencyList = getParallelAdjacencyList(input);
+            long endTime = System.currentTimeMillis();
+            long diffTime = endTime - startTime;
+    	    System.out.println("Lists obtained in " + diffTime);
 
-        startTime = System.currentTimeMillis();
-        
-        for (int i=0; i < adjacencyList.size()-1; i++) {
-        	
-        	int vertex1 = i;
-        	
-        	for(int j=0; j < adjacencyList.get(i).size()-1; j++) {
-        		int vertex2 = adjacencyList.get(i).get(j);
-        		if (vertex1 < vertex2) {
-        			int count = j+1;
-        		
-        			boolean same_vertex = true;
-        			while(same_vertex) {
-	        			int vertex3 = adjacencyList.get(i).get(count);
-	        			if( adjacencyList.get(vertex2).contains(vertex3) ) {
-	        				ret.add( new Triangle(vertex1, vertex2, vertex3) );
-	        			}
-	
-	        			count++;
-	        			if(count >= adjacencyList.get(i).size() ){
-	        				same_vertex = false;
-	        			}
-	        		}
-        		}
-        	}
-        }
-        
-        endTime = System.currentTimeMillis();
-        diffTime = endTime - startTime;
-	    System.out.println("Count obtained in " + diffTime);
+            startTime = System.currentTimeMillis();
+            
+            for (int i=0; i < adjacencyList.size()-1; i++) {
+            	class eachVertex implements Runnable {
+            		int i;
+            		ArrayList<Triangle> ret;
+            		ArrayList<Set<Integer>> adjacencyList;
+
+            		public eachVertex(int i, ArrayList<Triangle> ret, ArrayList<Set<Integer>> adjacencyList) {
+            			this.i = i;
+            			this.ret = ret;
+            			this.adjacencyList = adjacencyList;
+            		}
+    			public void run() {
+    				int vertex1 = i;
+    				
+    				if(adjacencyList.get(vertex1).size() > 1){
+    					Object[] vertex1Array = adjacencyList.get(i).toArray();
+    	            	for(int j=0; j < vertex1Array.length; j++) {
+    	            		int vertex2 = (int)vertex1Array[j];
+    	            		if(adjacencyList.get(vertex2).size() > 1){
+    	            			if (vertex1 < vertex2) {
+    	        			
+    	            				for(int count=j+1; count<vertex1Array.length; count++) {        			
+    	            					int vertex3 = (int)vertex1Array[count];
+    	            					if(vertex1 < vertex3){
+    	            						if( adjacencyList.get(vertex2).contains(vertex3) ) {
+    	            							if( vertex2 < vertex3 ) {
+    	            								ret.add( new Triangle(vertex1, vertex2, vertex3) );
+    	            							}else{
+    	            								ret.add( new Triangle(vertex1, vertex3, vertex2) );
+    	            							}
+    	            						}
+    	            					}        			
+    	            				}
+    	            			}
+    	            		}
+    	            	
+	            	/*for(int j=0; j < adjacencyList.get(i).size()-1; j++) {
+	            		int vertex2 = adjacencyList.get(i).get(j);
+	            		if (vertex1 < vertex2) {
+	            			int count = j+1;
+	            		
+	            			boolean same_vertex = true;
+	            			while(same_vertex) {
+	    	        			int vertex3 = adjacencyList.get(i).get(count);
+	    	        			if( adjacencyList.get(vertex2).contains(vertex3) ) {
+	    	        				ret.add( new Triangle(vertex1, vertex2, vertex3) );
+	    	        			}
+	    	
+	    	        			count++;
+	    	        			if(count >= adjacencyList.get(i).size() ){
+	    	        				same_vertex = false;
+	    	        			}
+	    	        		}
+	            		}
+	            	}*/
+    				}
+    				}
+    			}
+    		}
+    		pool.submit(new eachVertex(i, ret, adjacencyList));
+            	
+            }
+    		pool.shutdown();
+    		try {
+                pool.awaitTermination(1,TimeUnit.DAYS);
+            } catch (InterruptedException e) {
+                System.out.println("Pool interrupted!");
+                System.exit(1);
+            }
+    	}
 	    
         return ret;
     }
 
-    public ArrayList<ArrayList<Integer>> getAdjacencyList(byte[] data) throws IOException {
+    public ArrayList<HashSet<Integer>> getSingleAdjacencyList(byte[] data) throws IOException {
 	    InputStream istream = new ByteArrayInputStream(data);
 	    BufferedReader br = new BufferedReader(new InputStreamReader(istream));
 
@@ -85,37 +177,81 @@ public class TriangleCountImpl {
 	    int numEdges = Integer.parseInt(parts[1].split(" ")[0]);
 	    System.out.println("Found graph with " + numVertices + " vertices and " + numEdges + " edges");
  
-	    ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<ArrayList<Integer>>(numVertices);
+	    ArrayList<HashSet<Integer>> adjacencyList = new ArrayList<HashSet<Integer>>(numVertices);
 	
         while ((strLine = br.readLine()) != null && !strLine.equals(""))   {
-        	adjacencyList.add(new ArrayList<Integer>());
+        	adjacencyList.add(new HashSet<Integer>());
 	        parts = strLine.split(": ");
 	        int vertex = Integer.parseInt(parts[0]);
 	        if (parts.length > 1) {
 		        parts = parts[1].split(" ");
-                if (parts.length == 0 || parts.length == 1) {
-                	//do nothing
-                } else if (parts.length == 2 && (
-                		(Integer.parseInt(parts[0]) < vertex &&
-                				adjacencyList.get(Integer.parseInt(parts[0])).size() < 2) ||
-                		(Integer.parseInt(parts[1]) < vertex &&
-                		        adjacencyList.get(Integer.parseInt(parts[1])).size() < 2) ) ) {
-                	//do nothing
-                } else {
-		            for (String part: parts) {
-		                adjacencyList.get(vertex).add(Integer.parseInt(part));
-		            }
-                }
+		        for (String part: parts) {
+		        	adjacencyList.get(vertex).add(Integer.parseInt(part));
+		        }
 	        }
-	    }
-	    for (int i = 0; i < numVertices; i++) {
-	       Collections.sort(adjacencyList.get(i));
+        
 	    }
 	    br.close();
 
-        //for (int i = 0; i< adjacencyList.size(); i++ ) {
-         //  System.out.println(adjacencyList.get(i));
-        //}
 	    return adjacencyList;
+    }
+    
+    public ArrayList<Set<Integer>> getParallelAdjacencyList(byte[] data) throws IOException {
+    	ExecutorService pool = Executors.newFixedThreadPool(numCores);
+    	
+    	InputStream istream = new ByteArrayInputStream(input);
+    	BufferedReader br = new BufferedReader(new InputStreamReader(istream));
+    	String strLine = br.readLine();
+    	if (!strLine.contains("vertices") || !strLine.contains("edges")) {
+    	    System.err.println("Invalid graph file format. Offending line: " + strLine);
+    	    System.exit(-1);	    
+    	}
+    	String parts[] = strLine.split(", ");
+    	int numVertices = Integer.parseInt(parts[0].split(" ")[0]);
+    	int numEdges = Integer.parseInt(parts[1].split(" ")[0]);
+    	System.out.println("Found graph with " + numVertices + " vertices and " + numEdges + " edges");
+     
+    	ArrayList<Set<Integer>> adjacencyList = new ArrayList<Set<Integer>>(numVertices);
+    	for (int i = 0; i < numVertices; i++) {
+    		Set<Integer> mySet = Collections.newSetFromMap(new ConcurrentHashMap<Integer, Boolean>());
+    	    adjacencyList.add(mySet);
+    	}
+    	while ((strLine = br.readLine()) != null && !strLine.equals(""))   {
+    	//----------------------------------------------------------------------------------------------------------
+    		class eachLine implements Runnable {
+    			String parts[];
+    			String strLine;
+    			
+    			ArrayList<Set<Integer>> adjacencyList;
+
+    			public eachLine(String parts[], String strLine, ArrayList<Set<Integer>> adjacencyList) {
+    				this.parts = parts;
+    				this.strLine = strLine;
+    				this.adjacencyList = adjacencyList;
+    			}
+    		
+    			public void run() {
+    				parts = strLine.split(": ");
+    				int vertex = Integer.parseInt(parts[0]);
+    				if (parts.length > 1) {
+    					parts = parts[1].split(" +");
+    					for (String part: parts) {
+    						adjacencyList.get(vertex).add(Integer.parseInt(part));
+    					}
+    				}
+    			}
+    		}
+    		pool.submit(new eachLine(parts, strLine, adjacencyList));
+    	}
+    	pool.shutdown();
+    	try {
+    		pool.awaitTermination(1,TimeUnit.DAYS);
+    	} catch (InterruptedException e) {
+    		System.out.println("Pool interrupted!");
+    		System.exit(1);
+    	}
+    	//----------------------------------------------------------------------------------------------------------------
+    	br.close();
+    	return adjacencyList;
     }
 }
