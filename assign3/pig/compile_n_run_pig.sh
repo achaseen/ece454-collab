c1e2a52994cbf32dc3c6ae81be9843669ef0c5b5
achaseen <achaseen@uwaterloo.ca>
2015-07-21 01:33:08 -0400

    Update compile_n_run_pig.sh
    Updated the classpath line

--- assign3/pig/compile_n_run_pig.sh
+++ assign3/pig/compile_n_run_pig.sh
@@ -8,12 +8,13 @@ File=$1
 InputDir=$2
 
 # clean up
-hdfs dfs -rm -r -skipTrash /user/username/output
+hdfs dfs -rm -r -skipTrash /user/achaseen/output
 rm $File.jar
 rm $File*.class
 
-scp daniel@192.168.1.19:/home/daniel/school/ece454/assign3/ece454-collab/assign3/pig/$File\.java .
-scp daniel@192.168.1.19:/home/daniel/school/ece454/assign3/ece454-collab/assign3/pig/$File\.pig .
+export CLASSPATH=$(hadoop classpath):/usr/hdp/current/pig-client/pig-0.14.0.2.2.4.2-2-core-h2.jar
+scp achaseen@192.168.241.128:/home/achaseen/pig/$File\.java .
+scp achaseen@192.168.241.128:/home/achaseen/pig/$File\.pig .
 javac $File\.java
 jar -cf $File.jar $File*.class
-pig -param input=$inputDir -param output=output $File\.pig
+pig -param input=$InputDir -param output=output $File\.pig
